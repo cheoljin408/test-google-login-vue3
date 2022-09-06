db1d5c86f837058fc2a4860f71e4b06ed323ab74
cheoljin408 <kcj409@naver.com>
2022-09-06 16:17:11 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GoogleLogin msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GoogleLogin from './components/googleLogin.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GoogleLogin
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vue3GoogleLogin from 'vue3-google-login'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(vue3GoogleLogin, {
+    clientId: '782440341476-sqh3gbcq7c2egacram5c2m6tpe06e2fe.apps.googleusercontent.com'
+})
+
+app.mount('#app')
